0d35dbc7f225cb1642119652a4800aff7073cebe
Wanottah <wanottah@gmail.com>
2024-01-29 10:59:31 +0000

    Code comments and revision

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// array to store the board 
 let origBoard;
 
-let humanPlayer; // = 'O';
+// symbol select
+let playerSymbol;
 
-let aiPlayer; // = 'X';
+// human player
+let humanPlayer;
 
+// ai player
+let aiPlayer;
+
+// array to store the winning combinations of the game
 const winCombos = 
 [
 	// horizontal lines
@@ -21,57 +28,134 @@ const winCombos =
 	[6, 4, 2]
 ]
 
+// empty cell
+const EMPTY_CELL = "";
+
+// creates an array of positions on the board by
+// selecting all of the html elements with the class named '.cell'
 const cells = document.querySelectorAll('.cell');
 
-startGame();
+// get a reference to the 'restart' button
+const restartButton = document.querySelector("#restartButton");
+
+// get a reference to the 'X' button
+const xButton = document.querySelector("#xButton");
+
+// get a reference to the 'O' button
+const oButton = document.querySelector("#oButton");
+
+const selectorPanel_UI = document.querySelector('.playerSymbolPanel'); //.style.display = "none";
+
+const gameBoard_UI = document.querySelector('#gameBoard'); //.style.display = "block";
 
 
 
-function selectSym(sym)
+
+
+// start the game
+initialiseGame();
+
+
+
+function initialiseGame()
 {
-	humanPlayer = sym;
+	// add a 'click' event listener to the restart button
+	restartButton.addEventListener("click", resetGame);
 
-	aiPlayer = sym === 'O' ? 'X' :'O';
+	// add a 'click' event listener to the xbutton button
+	xButton.addEventListener("click", playerX);
 
-	origBoard = Array.from(Array(9).keys());
+	// add a 'click' event listener to the obutton button
+	oButton.addEventListener("click", playerO);
 
-	for (let i = 0; i < cells.length; i++) 
-	{
-	  cells[i].addEventListener('click', turnClick, false);
-	}
 
-	if (aiPlayer === 'X') 
-	{
-	  turn(bestSpot(),aiPlayer);
-	}
-
-	// hide the player symbol select panel
-	document.querySelector('.selectSym').style.display = "none";
-
-	// show the game board
-	document.querySelector('.gameBoard').style.display = "block";
+	// reset the game
+	resetGame();
 }
 
 
-function startGame() 
+function playerX()
 {
-	// hide the game board
-	document.querySelector('.gameBoard').style.display = "none";
+	playerSymbol = "X";
 
+	startGame(playerSymbol);
+}
+
+
+function playerO()
+{
+	playerSymbol = "O";
+
+	startGame(playerSymbol);
+}
+
+
+function startGame(playerSymbol)
+{
+	// hide the player symbol select panel
+	selectorPanel_UI.style.display = "none";
+	
+	// show the game board
+	gameBoard_UI.style.display = "block";
+
+	// assign the selected symbol to the human player
+	humanPlayer = playerSymbol;
+
+	// assign the remaining symbol to the ai player
+	// if player has selected 'O'
+	// then,
+	// ai = 'X'
+	// otherwise,
+	// ai = 'O'
+	aiPlayer = playerSymbol === 'O' ? 'X' : 'O';
+
+	// loop through all of the positions in the 'cells' array and add a 'click' event listener
+	for (let i = 0; i < cells.length; i++) 
+	{
+		cells[i].addEventListener('click', turnClick, false);
+	}
+
+	// if the human player has selected 'O'
+	// then ai makes the first move
+	if (aiPlayer === 'X') 
+	{
+	  	turn(bestSpot(), aiPlayer);
+	}
+}
+
+
+function resetGame() 
+{
+	// hide the game state text
 	document.querySelector(".endgame").style.display = "none";
-  	
-	document.querySelector('.endgame .text').innerText = "";
-  	
-	document.querySelector('.selectSym').style.display = "block";
 
-	origBoard = Array.from(Array(9).keys());
+	// hide the restart button
+	document.querySelector('.gameButtonPanel').style.display = "none";
 
-	for (var i = 0; i < cells.length; i++) 
+	// hide the game board
+	document.querySelector('#gameBoard').style.display = "none";
+
+	// clear the game board
+	for (let i = 0; i < cells.length; i++) 
   	{
-		cells[i].innerText = '';
+		cells[i].innerText = EMPTY_CELL;
 
 		cells[i].style.removeProperty('background-color');
 	}
+
+	// initialise the game board with the numbers 0 - 8
+	// stores position of current player
+	origBoard = 
+	[
+		0, 1, 2,
+
+		3, 4, 5,
+
+		6, 7, 8
+	];
+  	
+	// show the select player symbol panel
+	document.querySelector('.playerSymbolPanel').style.display = "block";
 }
 
 
@@ -126,47 +210,11 @@ function checkWin(board, player)
 }
 
 
-function gameOver(gameWon) 
-{
-	for (let index of winCombos[gameWon.index]) 
-  	{
-		document.getElementById(index).style.backgroundColor = gameWon.player == humanPlayer ? "blue" : "red";
-	}
-
-	for (var i = 0; i < cells.length; i++) 
-  	{
-		cells[i].removeEventListener('click', turnClick, false);
-	}
-
-	declareWinner(gameWon.player == humanPlayer ? "You win!" : "You lose.");
-}
-
-
-function declareWinner(who) 
-{
-	document.querySelector(".endgame").style.display = "block";
-
-	document.querySelector(".endgame .text").innerText = who;
-}
-
-
-function emptySquares() 
-{
-	return origBoard.filter(s => typeof s == 'number');
-}
-
-
-function bestSpot() 
-{
-	return minimax(origBoard, aiPlayer).index;
-}
-
-
 function checkTie() 
 {
 	if (emptySquares().length == 0) 
   	{
-		for (var i = 0; i < cells.length; i++) 
+		for (let i = 0; i < cells.length; i++) 
     	{
 			cells[i].style.backgroundColor = "green";
 
@@ -182,9 +230,48 @@ function checkTie()
 }
 
 
+function gameOver(gameWon) 
+{
+	for (let index of winCombos[gameWon.index]) 
+  	{
+		document.getElementById(index).style.backgroundColor = gameWon.player == humanPlayer ? "blue" : "red";
+	}
+
+	for (let i = 0; i < cells.length; i++) 
+  	{
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+
+	declareWinner(gameWon.player == humanPlayer ? "You win!" : "You lose.");
+}
+
+
+function declareWinner(who) 
+{
+	document.querySelector(".endgame").style.display = "block";
+
+	document.querySelector(".endgame .text").innerText = who;
+
+	// show the restart button
+	document.querySelector('.gameButtonPanel').style.display = "block";
+}
+
+
+function emptySquares() 
+{
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+
+function bestSpot() 
+{
+	return minimax(origBoard, aiPlayer).index;
+}
+
+
 function minimax(newBoard, player) 
 {
-	var availSpots = emptySquares();
+	let availSpots = emptySquares();
 
 	if (checkWin(newBoard, humanPlayer)) 
   	{
@@ -201,11 +288,11 @@ function minimax(newBoard, player)
 		return {score: 0};
 	}
 
-	var moves = [];
+	let moves = [];
 
-	for (var i = 0; i < availSpots.length; i++) 
+	for (let i = 0; i < availSpots.length; i++) 
   	{
-		var move = {};
+		let move = {};
 
 		move.index = newBoard[availSpots[i]];
 
@@ -213,14 +300,14 @@ function minimax(newBoard, player)
 
 		if (player == aiPlayer) 
     	{
-			var result = minimax(newBoard, humanPlayer);
+			let result = minimax(newBoard, humanPlayer);
 
 			move.score = result.score;
 		} 
 		
 		else 
 		{
-			var result = minimax(newBoard, aiPlayer);
+			let result = minimax(newBoard, aiPlayer);
 
 			move.score = result.score;
 		}
@@ -230,13 +317,15 @@ function minimax(newBoard, player)
 		moves.push(move);
 	}
 
-	var bestMove;
+	// if it is the computer's turn
+	let bestMove;
 
 	if(player === aiPlayer) 
   	{
-		var bestScore = -10000;
+		let bestScore = -10000;
 
-		for(var i = 0; i < moves.length; i++) 
+		// loop through the available moves and choose the move with the highest score
+		for(let i = 0; i < moves.length; i++) 
     	{
 			if (moves[i].score > bestScore) 
       		{
@@ -247,11 +336,12 @@ function minimax(newBoard, player)
 		}
 	} 
   
+	// otherwise,
   	else 
   	{
-		var bestScore = 10000;
+		let bestScore = 10000;
 
-		for(var i = 0; i < moves.length; i++) 
+		for(let i = 0; i < moves.length; i++) 
 		{
 			if (moves[i].score < bestScore) 
 			{
